c48a354a03fd8ae1a55b65bae106fc536a289185
ParagJn <parag.jn@gmail.com>
2025-05-15 23:19:23 +0530

    changes to configure github spaces

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', // Enable static export for GitHub Pages/Spaces
+  basePath: '/parag_space', // Set basePath for GitHub Pages
+  assetPrefix: '/parag_space', // Set assetPrefix for GitHub Pages
   /* config options here */
 };
 
